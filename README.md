22c4de25d8ce0f69be7312afae0d6d7cae50aa75
jeanp413 <jeanp413@hotmail.com>
2021-09-01 20:58:02 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Smoke Test Check Web Extension
+
+This is a test extension used for smoke testing VS Code Web Extensions UI.
---
+++ src/web/extension.ts
@@ -0,0 +1,11 @@
+import * as vscode from 'vscode';
+
+export function activate(context: vscode.ExtensionContext) {
+
+	context.subscriptions.push(vscode.commands.registerCommand('smoketest-check-web.smoketestCheckWeb', () => {
+		vscode.window.showInformationMessage('Hello World from smoketest-check-web!');
+	}));
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {}
